bec363c9cfdcb607f30c33a901d619d7aa8c8e9f
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-22 15:42:41 +0100

    somm a e differenza

---
+++ Helper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcoliHelper
+{
+    public class Helper
+    {
+        public static int Somma(int num1, int num2) => num1 + num2;
+        public static double Somma(double num1, double num2) => num1 + num2;
+
+        public static void Differenza(int num1,int num2)
+        {
+            Console.WriteLine("inserisci il primo numero");
+            
+
+            if (num1 < num2)
+            {
+                Console.WriteLine("il primo numero deve essere maggiore del secondo");
+            }else if (num1 - num2 == 0)
+            {
+                Console.WriteLine("non c'è differenza");
+            }
+            else
+            {
+
+            Console.WriteLine($"la differenza tra {num1} e {num2} è {num1 - num2}");
+            }
+            
+
+        }
+        public static void Differenza(double num1, double num2)
+        {
+            Console.WriteLine("inserisci il primo numero");
+
+
+            if (num1 < num2)
+            {
+                Console.WriteLine("il primo numero deve essere maggiore del secondo");
+            }
+            else if (num1 - num2 == 0)
+            {
+                Console.WriteLine("non c'è differenza");
+            }
+            else
+            {
+
+                Console.WriteLine($"la differenza tra {num1} e {num2} è {num1 - num2}");
+            }
+
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using CalcoliHelper;
+//Somma di due numeri interi:
+Console.WriteLine(Helper.Somma(1, 3));
+
+//Somma di due numeri double:
+Console.WriteLine(Helper.Somma(1.5, 3.3));
+
+//Differenza tra due numeri interi
+Helper.Differenza(8,3);
