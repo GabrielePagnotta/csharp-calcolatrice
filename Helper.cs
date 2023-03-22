e7095d8ed22ff1800761923121cbd39ecaf4bfc2
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-22 16:27:50 +0100

    fine esercizio base

--- Helper.cs
+++ Helper.cs
@@ -8,50 +8,66 @@ namespace CalcoliHelper
 {
     public class Helper
     {
+        
         public static int Somma(int num1, int num2) => num1 + num2;
         public static double Somma(double num1, double num2) => num1 + num2;
 
-        public static void Differenza(int num1,int num2)
-        {
-            Console.WriteLine("inserisci il primo numero");
-            
+        public static int Differenza(int num1, int num2) => num1 - num2;
+        
+        public static double Differenza(double num1, double num2) => num1 - num2;
+        
 
+        public static int Moltiplicazione(int num1, int num2) => num1 * num2;
+
+        public static double Moltiplicazione(double num1, double num2) => num1 * num2;
+
+        public static int Assoluto(int num1) => Math.Abs(num1);
+        public static double Assoluto(double num1) => Math.Abs(num1);
+
+        public static int Minimo(int num1, int num2)
+        {
             if (num1 < num2)
             {
-                Console.WriteLine("il primo numero deve essere maggiore del secondo");
-            }else if (num1 - num2 == 0)
-            {
-                Console.WriteLine("non c'è differenza");
+                return num1;
             }
             else
             {
-
-            Console.WriteLine($"la differenza tra {num1} e {num2} è {num1 - num2}");
+                return num2;
             }
-            
-
         }
-        public static void Differenza(double num1, double num2)
+        public static double Minimo(double num1, double num2)
         {
-            Console.WriteLine("inserisci il primo numero");
-
-
             if (num1 < num2)
             {
-                Console.WriteLine("il primo numero deve essere maggiore del secondo");
-            }
-            else if (num1 - num2 == 0)
-            {
-                Console.WriteLine("non c'è differenza");
+                return num1;
             }
             else
             {
-
-                Console.WriteLine($"la differenza tra {num1} e {num2} è {num1 - num2}");
+                return num2;
             }
-
-
+        }
+        public static int Massimo(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
         }
 
+        public static double Massimo(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,4 +6,31 @@ Console.WriteLine(Helper.Somma(1, 3));
 Console.WriteLine(Helper.Somma(1.5, 3.3));
 
 //Differenza tra due numeri interi
-Helper.Differenza(8,3);
+Console.WriteLine(Helper.Differenza(8,3));
+
+//Differenza tra due numeri double
+Console.WriteLine(Helper.Differenza(8.5,3.4));
+
+//Moltiplicazione di due numeri interi
+Console.WriteLine(Helper.Moltiplicazione(8,3));
+
+//Moltiplicazione di due numeri double
+Console.WriteLine(Helper.Moltiplicazione(8.7, 3.5));
+
+//Valore assoluto di un numero intero
+Console.WriteLine(Helper.Assoluto(-12));
+
+//Valore assoluto di un numero double
+Console.WriteLine(Helper.Assoluto(-12.70));
+
+//Minimo tra due numeri interi
+Console.WriteLine(Helper.Minimo(5,9));
+
+//Minimo tra due numeri double
+Console.WriteLine(Helper.Minimo(5.8, 9.7));
+
+//Massimo tra due numeri interi
+Console.WriteLine(Helper.Massimo(10, 22));
+
+//Massimo tra due numeri double
+Console.WriteLine(Helper.Massimo(50.2, 32.4));
